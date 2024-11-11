75cf0118c56538c8c8a30393e02a67827c2faa31
Lucas Mendes <45476652+lucasfabbrim@users.noreply.github.com>
2024-11-11 09:47:27 -0300

    Update instagram.tsx

--- src/components/instagram.tsx
+++ src/components/instagram.tsx
@@ -140,9 +140,9 @@ export default function InstagramCarouselComponent() {
             Off season... Continue!
           </span>
         </span>
-        <span className="mt-3 text-sm font-semibold text-blue-300 flex items-center gap-1">
+        <span className="mt-3 text-xs font-semibold text-blue-400 flex items-center gap-1">
           @growthsupplements
-          <span className="text-sm font-normal text-white">| code: PH</span>
+          <span className="text-xs font-normal text-white">| code: PH</span>
         </span>
         <div className="text-xs text-zinc-400 mt-3 font-normal">
           28 de janeiro
