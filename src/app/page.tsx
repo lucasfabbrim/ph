3be36541c8020f80f62ff42687d11c0e2aab75c5
Lucas Mendes <45476652+lucasfabbrim@users.noreply.github.com>
2024-11-11 12:46:42 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,8 +87,8 @@ export default function Home() {
 
           {/* Excellence Card */}
           <div className="space-y-3 border-b border-b-zinc-900 pb-12 mx-6 pt-6 border-t border-t-zinc-900">
-            <h1 className="items-center text-center text-xl pb-4 font-semibold">
-              Organização & Produtividade
+            <h1 className="items-center text-center text-xl pb-5 font-semibold pt-4">
+              Organização
             </h1>
             <Card className="bg-neutral-900/60 border-none rounded-[25px]">
               <div className="w-full justify-between p-4 h-auto flex items-center">
